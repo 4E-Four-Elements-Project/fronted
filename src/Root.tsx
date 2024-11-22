87dd931f3b34b4ea37f66ebba9990e6301dc008d
Eric Frisk <eric.frisk@hotmail.com>
2024-11-22 15:23:52 +0100

    first commit

---
+++ src/Root.tsx
@@ -0,0 +1,11 @@
+function Root() {
+  return (
+    <>
+      <h1 className="text-purple-800 text-3xl font-bold">
+        Tailwind, framer-motion och react-router installerat
+      </h1>
+    </>
+  );
+}
+
+export default Root;
