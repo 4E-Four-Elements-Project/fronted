5b918baf88976aa215b4df666d99c813402eb71d
Eric Frisk <eric.frisk@hotmail.com>
2024-11-26 09:58:32 +0100

    Added react-router to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
 import Root from "./Root.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Root />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Root />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
