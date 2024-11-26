cd2a4a7eee367c3e929e0cd2a5ede27c5e653ead
christofferwallman <wallman92@gmail.com>
2024-11-26 09:31:30 +0100

    added images and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Roboto: ["Roboto", "sans-serif"],
+        Londrina: ["Londrina", "cursive"]},
+      colors: {
+        primary: ["#FFF6F5"],
+        secondary: ["#B8F9AD"],
+        pink: ["#E5B4DD"],
+        yellow: ["#FFEF3B"],
+        green: ["#C8D6AF "],
   },
+  },
+},
   plugins: [],
 };
